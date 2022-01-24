42ef3ffa51bf9487bd46bdd6f6f5bf16a6f46ec4
chlbri <51156116+chlbri@users.noreply.github.com>
2022-01-24 17:01:05 +0000

    Initial commit

---
+++ .gitpod-config/bash.sh
@@ -0,0 +1,2 @@
+yarn add -D typescript prettier jest @types/jest ts-jest jest-cucumber eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser shelljs edit-json-file @types/faker dayjs faker immer jest-extended nanoid rimraf
+yarn add dequal
